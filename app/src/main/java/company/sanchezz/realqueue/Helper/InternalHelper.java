c384815277445894817eefa4f30fec495bb758c7
Alejandro Sanchez <alejandro@axented.com>
2017-07-03 19:03:33 -0500

    Remove some kotlin parts

---
+++ app/src/main/java/company/sanchezz/realqueue/Helper/InternalHelper.java
@@ -0,0 +1,11 @@
+package company.sanchezz.realqueue.Helper;
+
+/**
+ * Created by AlejandroSanchez on 7/3/17.
+ */
+
+public class InternalHelper {
+
+    
+
+}
---
+++ app/src/main/java/company/sanchezz/realqueue/MainActivity.java
@@ -0,0 +1,13 @@
+package company.sanchezz.realqueue;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
